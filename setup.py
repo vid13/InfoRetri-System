c9447ca0912c4234dd164884a7c658ed6d48102a
Vid <vidvata@gmail.com>
2024-10-24 00:23:28 -0700

    added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'GenAI Info Retrieval Project',
+    version = "0.0.0",
+    auther = "vid",
+    auther_email = "vidvata@gmail.com",
+    package = find_packages(),
+    install_requires = []
+)
